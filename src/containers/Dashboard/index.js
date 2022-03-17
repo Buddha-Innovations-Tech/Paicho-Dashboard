f95a439d46541e61cd7f9fa95a3edd8227bb993b
Iamsindu <csitan.sindhu@gmail.com>
2022-03-17 12:04:16 +0545

    fetching data after registration

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -1,8 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { useNavigate, Outlet } from "react-router-dom";
 import { Row, Col } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+
 import SideBar from "../../components/SideBar";
 import NavBar from "../../components/NavBar";
 const Dashboard = () => {
+  const { userInfo } = useSelector((state) => state.userLogin);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!userInfo) {
+      navigate("/login");
+    }
+  }, [userInfo]);
+
   return (
     <>
       <Row className="gx-0">
