1efbe4b5e0d866919f5949f1ca364b09ca928c12
Akash Khattri <akash.khattri.ak@gmail.com>
2022-10-19 13:03:07 +0545

    done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./containers/App";
-import { BrowserRouter as Router } from "react-router-dom";
-import "../src/styles/App.scss";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Provider } from "react-redux";
-import store from "./store";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import '../src/styles/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
+import store from './store';
 
 ReactDOM.render(
   <Router>
@@ -13,5 +13,5 @@ ReactDOM.render(
       <App />
     </Provider>
   </Router>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
